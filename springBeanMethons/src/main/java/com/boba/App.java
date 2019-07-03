16a997c40e6e96296cb6d0df9af1518a6c7ca979
Boba-Syu <1914808114@qq.com>
2019-07-03 16:15:58 +0800

    springBean 定义继承

--- springBeanMethons/src/main/java/com/boba/App.java
+++ springBeanMethons/src/main/java/com/boba/App.java
@@ -1,6 +1,7 @@
 package com.boba;
 
 import com.boba.entity.People;
+import com.boba.entity.User;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -20,6 +21,7 @@ public class App {
         people1.setName("123");
         System.out.println(people1);
         System.out.println(people2);
+
         /*测试原型模式prototype, 每次获得的都是新对象 */
         System.out.println("----------prototype测试----------");
         people1 = (People) ac.getBean("people2");
@@ -29,12 +31,19 @@ public class App {
         people1.setName("123");
         System.out.println(people1);
         System.out.println(people2);
+
         /*测试原型模式prototype, 每次获得的都是新对象 */
         System.out.println("----------生命周期和后置处理器测试----------");
         AbstractApplicationContext aac = new ClassPathXmlApplicationContext("META-INF/applicationContext2.xml");
+
         /* 在容器创建前, 还未从容器中获取时, init-method就被调用 */
         People people3 = (People) aac.getBean("people3");
         System.out.println(people3);
         aac.registerShutdownHook(); //正常关闭, 触发调用的destory方法
+
+        /* bean继承 */
+        System.out.println("----------spring bean定义继承-------------");
+        User user = (User) ac.getBean("user");
+        System.out.println(user);
     }
 }
