292a209a196b1781de55ab575e2f6f8a72286086
zhoubo <bobasyu@163.com>
2019-07-15 23:52:53 +0800

    Mybatis的集中查询

--- mybatisLearning/src/main/java/com/boba/App.java
+++ mybatisLearning/src/main/java/com/boba/App.java
@@ -9,6 +9,7 @@ import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -22,12 +23,36 @@ public class App {
         SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
         SqlSession sqlSession = sqlSessionFactory.openSession();
 
+        //两种方式都行
         //List<User> users = sqlSession.selectList("com.boba.mapper.UserMapper.selectAll");
         List<User> users = (List<User>) sqlSession.getMapper(UserMapper.class).selectAll();
-        for(User user:users) {
+        for (User user : users) {
             System.out.println(user);
         }
 
+        //带有参数的查询语句
+        System.out.println("----------带有参数的查询语句----------");
+        User user = sqlSession.getMapper(UserMapper.class).selectById(1);
+        System.out.println(user);
+
+        //动态查询
+        System.out.println("---------------动态查询--------------");
+        User user1 = new User();
+        user1.setUid(1);
+        List<User> users1 = (List<User>) sqlSession.getMapper(UserMapper.class).select(user1);
+        for (User u : users1) {
+            System.out.println(u);
+        }
+
+        //foreach查询
+        System.out.println("-------------foreach查询-------------");
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        List<User> users2 = (List<User>) sqlSession.getMapper(UserMapper.class).selectForeach(list);
+        for (User u : users2) {
+            System.out.println(u);
+        }
         sqlSession.close();
     }
 }
--- mybatisLearning/src/main/java/com/boba/mapper/UserMapper.java
+++ mybatisLearning/src/main/java/com/boba/mapper/UserMapper.java
@@ -1,12 +1,17 @@
 package com.boba.mapper;
 
 import com.boba.entity.User;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
 public interface UserMapper {
     List<User> selectAll();
 
-    User selectById(int id);
+    User selectById(@Param("uid") int uid);
+
+    List<User> select(User user);
+
+    List<User> selectForeach(@Param("list") List list);
 
 }
